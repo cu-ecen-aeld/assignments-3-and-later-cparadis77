9a14976508ec2fcc2fd784489542989177943f21
Christian Paradis <cparadis@orthogone.com>
2023-03-05 13:01:03 -0500

    added implementation for do_exec()

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,10 @@
 #include "systemcalls.h"
 #include <stdlib.h>             // To use system()
+#include <unistd.h>             // To use fork()
+#include <sys/wait.h>           // To use wait()
+
+
+
 
 
 /**
@@ -18,13 +23,13 @@ bool do_system(const char *cmd)
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
-*/
+ */
 
     // Debug only
-    printf("Debug only: The command is \"%s\"\n", cmd);
+    printf("Debug only: do_system(): The command is \"%s\"\n", cmd);
 
     int iReturnCode = system(cmd);
-    printf("Debug only: iReturnCode is %d\n", iReturnCode);
+    printf("Debug only: do_system(): iReturnCode is %d\n", iReturnCode);
 
     // The return value of system() is one of the following:
     //
@@ -53,6 +58,10 @@ bool do_system(const char *cmd)
     return bRetValue;
 }
 
+
+
+
+
 /**
 * @param count -The numbers of variables passed to the function. The variables are command to execute.
 *   followed by arguments to pass to the command
@@ -69,6 +78,9 @@ bool do_system(const char *cmd)
 
 bool do_exec(int count, ...)
 {
+    bool bRetValue = false;     // Assume error state at first.
+    printf("Debug only: do_exec(): The count is %d\n", count);
+
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -77,12 +89,16 @@ bool do_exec(int count, ...)
     {
         command[i] = va_arg(args, char *);
         // Debug only
-        printf("Debug only: The command%d is %s\n", i, command[i]);
+        printf("Debug only: do_exec(): The command%d is %s\n", i, command[i]);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
+
+    va_end(args);
+
+
 
 /*
  * TODO:
@@ -92,21 +108,67 @@ bool do_exec(int count, ...)
  *   (first argument to execv), and use the remaining arguments
  *   as second argument to the execv() command.
  *
+ * 
+ * --- notes from CParadis:
+ * 
  *      pid_t fork(void);
+ *          RETURN VALUE
+ *              In parent process, -1 on error, or the Child PID on success
+ *              In child process, it is 0
+ *              
  *      int execv(const char *path, char *const argv[]);
  *           RETURN VALUE
  *               A successful call to execv does not have a return value because the new process image overlays the calling process image. 
  *               However, a -1 is returned if the call to execv is unsuccessful.
+ * 
  *      pid_t wait(int *wstatus);
  *              wait(): on success, returns the process ID of the terminated child; on failure, -1 is returned.
- *                  int wstatus;        wait(&wstatus)
+ *              int wstatus;        wait(&wstatus)
+ *                  On success, wstatus shall be 0, and non-zero on failure
  * 
  * Reference: https://support.sas.com/documentation/onlinedoc/sasc/doc/lr2/execv.htm
-*/
+ * 
+ */
 
-    va_end(args);
+    pid_t childPid;
+    int execvRetCode;
+    pid_t waitedChildPid;
+    int waitStatus;
+    if ((childPid = fork()) == -1 )
+    {
+        perror("can't fork");
+    }
+    else if (childPid == 0)
+    {
+        // Let Child Process to execute any work
+        execvRetCode = execv(command[0], command);
+        printf("Debug only: do_exec(): The return code is %d, but return is not expected on success\n", execvRetCode);
+    }
+    else
+    {
+        // Let Parent process to finish any work
+        printf("Debug only: do_exec(): The Child PID is %d\n", childPid);
+        
+        // Waiting on Child process
+        waitedChildPid = wait(&waitStatus);
+        
+        if (waitedChildPid == -1)
+        {
+            perror("Got error while waiting on Child process...");
+        }
+        else
+        {
+            printf("Debug only: do_exec(): The Waited Child PID is %d\n", waitedChildPid);
+            printf("Debug only: do_exec(): The Wait status is %d\n", waitStatus);
 
-    return true;
+            if ((childPid == waitedChildPid) && (waitStatus == 0))
+            {
+                bRetValue = true;
+            }
+        }
+    }
+
+    return bRetValue;
 }
 
 /**
@@ -116,6 +178,10 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+    bool bRetValue = false;     // Assume error state at first.
+    printf("Debug only: do_exec_redirect(): The outputfile is %s\n", outputfile);
+    printf("Debug only: do_exec_redirect(): The count is %d\n", count);
+
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -124,7 +190,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     {
         command[i] = va_arg(args, char *);
         // Debug only
-        printf("Debug only: The command%d is %s\n", i, command[i]);
+        printf("Debug only: do_exec_redirect(): The command%d is %s\n", i, command[i]);
     }
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
@@ -146,5 +212,6 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
 
     va_end(args);
 
-    return true;
+    bRetValue = true;   // TEMPORARY only!!!!
+    return bRetValue;
 }
