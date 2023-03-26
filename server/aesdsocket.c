437c1a71ca3a9c402aa2c06160aa47e6258ded5c
Christian Paradis <cparadis@orthogone.com>
2023-03-26 11:27:04 -0400

    Clean up in progress...still working good

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -8,19 +8,19 @@
 //
 //      Note: The application handle SIGINT and SIGTERM using a sigaction
 //      
-//      TODO: need to support multiple client and use non-blocking io.........InProgress
-//      TODO: need to handle data from socket.................................InProgress
-//      TODO: need to write data into a file..................................TBD
-//      TODO: need to send data back to the client............................TBD
+//      TODO: need to support client and use non-blocking socket..............DONE  (we do not need to support multiple client at the same time)
+//      TODO: need to handle data from socket.................................DONE
+//      TODO: need to write data into a file..................................DONE
+//      TODO: need to send the full file content back to the client...........DONE (only when the received packet is completed)
 //      TODO: need to handle argument -d and launch it as a daemon............TBD
-//      TODO: do we need to support multiple client???........................TBD
 //
 //  Note: In short, ssize_t is the same as size_t, but is a signed type - read ssize_t as
 //          “signed size_t”. ssize_t is able to represent the number -1, which is returned by
 //          several system calls and library functions as a way to indicate error. For example, the
 //          read and write system calls
 //
-//          size_t == long unsigned int
+//              size_t  == long unsigned int
+//              ssize_t == long signed int 
 //
 //  Usage:  ./aesdsocket            To run it without a daemon
 //          ./aesdsocket -d         To run it as a deamon
@@ -37,7 +37,7 @@
 #include <syslog.h>
 #include <signal.h>
 #include <errno.h>
-#include <fcntl.h>              // To use fcntl()
+#include <fcntl.h>              // To use fcntl() for socket option
 #include <assert.h>
 
 // -- Global Constants ----------------------------------------------------------------------------
@@ -45,30 +45,66 @@
 #define CONNECTION_QUEUE 3
 #define OUTPUT_FILE "/var/tmp/aesdsocketdata"
 #define RECV_BUFFER_SIZE 1024
-//#define PROC_BUFFER_SIZE 1024
+
+// Optional: use these functions to add debug or error prints to your application
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("LOG_DEBUG: " msg "\n" , ##__VA_ARGS__)
+#define ERROR_LOG(msg,...) printf("LOG_ERROR: " msg "\n" , ##__VA_ARGS__)
+
 
 // -- Flags ---------------------------------------------------------------------------------------
-volatile int running = 1;               // TODO: change flag for bool instead of int
+volatile int running = 1;                       // TODO: change flag for bool instead of int
 
 bool caught_sigint = false;
 bool caught_sigterm = false;
-//bool caught_sigkill = false;        // Note that SIGKILL signal can not be caught and handle!!
+//bool caught_sigkill = false;                  // Note that SIGKILL signal can not be caught and handle!!
 int  caught_another_sig = -1;
 
+
+
+//////////////////////////////////////////
+// Register signal handlers for SIGTERM, SIGINT, and SIGQUIT
+//
+// Note that we can not register a signal handler for SIGKILL
+//////////////////////////////////////////
+void register_signal_handler(struct sigaction *sa)
+{
+    DEBUG_LOG("register_signal_handler()...");
+
+    // Ensure valid param
+    assert(sa != NULL); 
+
+    // Register signal handlers for SIGTERM, SIGINT, SIGKILL, and SIGQUIT
+    if (sigaction(SIGTERM, sa, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Failed registering for SIGTERM, errno %d (%s)", errno, strerror(errno));
+    }
+
+    if (sigaction(SIGINT, sa, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Failed registering for SIGINT, errno %d (%s)", errno, strerror(errno));
+    }
+
+    if (sigaction(SIGQUIT, sa, NULL) != 0)
+    {
+        syslog(LOG_ERR, "Failed registering for SIGQUIT, errno %d (%s)", errno, strerror(errno));
+    }
+
+}
 
 //////////////////////////////////////////
 // Signal handler function
 //////////////////////////////////////////
 void handle_signal(int signal_number)
 {
-    printf("handle_signal()...got signal_number=%d\n", signal_number);            // To be commentted out!!!
+    DEBUG_LOG("handle_signal()...got signal_number=%d", signal_number);
 
     // Flag which signal we received
     if (signal_number == SIGINT)
         caught_sigint = true;
     else if (signal_number == SIGTERM)
         caught_sigterm = true;
-    //else if (signal_number == SIGKILL)                  // Note that SIGKILL signal can not be caught and handle!!
+    //else if (signal_number == SIGKILL)        // Note that SIGKILL signal can not be caught and handle!!
     //    caught_sigkill = true;
     else
         caught_another_sig = signal_number;
@@ -82,30 +118,38 @@ void handle_signal(int signal_number)
 //////////////////////////////////////////
 void print_signal_infos()
 {
-    printf("print_signal_infos()...\n");            // To be commentted out!!!
+    DEBUG_LOG("print_signal_infos()...");
 
     if (caught_sigint)
-        printf("\nCaught SIGINT!\n");
+        DEBUG_LOG("print_signal_infos()...Caught SIGINT!");
 
     if (caught_sigterm)
-        printf("\nCaught SIGTERM!\n");
-
-    //if (caught_sigkill)                                   // Note that SIGKILL signal can not be caught and handle!!
-    //    printf("\nCaught SIGKILL!\n");
+        DEBUG_LOG("print_signal_infos()...Caught SIGTERM!");
 
     if (caught_another_sig != -1)
-        printf("\nCaught Signal no %d\n", caught_another_sig);
+        DEBUG_LOG("print_signal_infos()...Caught Signal no %d", caught_another_sig);
+
+    syslog(LOG_ERR, "Caught signal, exiting");
 }
 
 
+//////////////////////////////////////////
+// Function to delete the output file
+//////////////////////////////////////////
 void delete_output_file()
 {
+    DEBUG_LOG("delete_output_file()...");
+
     int result = remove(OUTPUT_FILE);
 
-    if (result == 0) {
-        printf("File deleted successfully.\n");
-    } else {
-        printf("Error deleting file.\n");
+    if (result == 0)
+    {
+        DEBUG_LOG("delete_output_file()...File deleted successfully.");
+    }
+    else
+    {
+        ERROR_LOG("delete_output_file()...Failed to delete file %s, errno %d (%s)", OUTPUT_FILE, errno, strerror(errno));
+        syslog(LOG_ERR, "Failed to delete file %s, errno %d (%s)", OUTPUT_FILE, errno, strerror(errno));
     }
 }
 
@@ -113,112 +157,78 @@ void delete_output_file()
 //////////////////////////////////////////
 // Main Server entry point
 //////////////////////////////////////////
-//int main()
 int main(int argc, char *argv[])
 {
-    FILE *pFile;		                // Creating file pointer to work with files
-    int socket_fd;                      // Socket file descriptor used by the Server                                        //server_fd;
-    int new_socket = -1;                // Socket file descriptor used to communicate with a client application         // *** to be rename client_socket ***
-    struct sockaddr_in sSockAddress;                                                                                        //address
+    FILE *pFile;		                    // Creating file pointer to work with files
+    int socket_fd;                          // Socket file descriptor used by the Server                                        //server_fd;
+    int new_socket = -1;                    // Socket file descriptor used to communicate with a client application         // *** to be rename client_socket ***
+    struct sockaddr_in sSockAddress;        // Socket addr structure
     int opt = 1;
     int addrlen = sizeof(sSockAddress);
-    //char buffer[1024] = {0};            // TO be remove
+    //char buffer[1024] = {0};              // To be remove
     char recv_buffer[RECV_BUFFER_SIZE] = {0};
-    //char proc_buffer[PROC_BUFFER_SIZE] = {0};
-    //char bufferTmp[1024] = {0};
-    //char bufferTmp[10] = {'H', 'e', 'l', 'l', 'o', '\0'};
-    //const char *hello = "Hello from server";
     bool bRunAsDaemon = false;
-    //bool bSigHandlerSuccess = true;
-    int iUsleepRc = 0;
     ssize_t num_bytes_received;
-    ssize_t num_bytes_sent;
-
-    char client_ip[INET_ADDRSTRLEN];
-    int client_port = -1;
+    
+    char client_ip[INET_ADDRSTRLEN];        // Used to extract client IP addr
+    int client_port = -1;                   // Used to extract the client port
 
     bool bSendDataBackToClient = false;
+    ssize_t num_bytes_sent;
 
+    int iUsleepRc = 0;                      // TODO: to be remove!!
+    
 
-    printf("main()...entry point...argc=%d\n", argc);            // To be commentted out!!!
+    DEBUG_LOG("main()...entry point...argc=%d", argc);
 
     // Open syslog
     openlog(NULL, 0, LOG_USER);
-
-    syslog(LOG_DEBUG, "main()...entry point...argc=%d", argc);
-
+    
+    // Handle parameters
     if (argc > 2)
     {
-        fprintf(stderr, "Usage: %s.........to run it as an application\n", argv[0]);
-        fprintf(stderr, "Usage: %s -d......to run it as a daemon\n", argv[0]);
-
+        printf("Usage: %s          to run it as an application\n", argv[0]);
+        printf("Usage: %s -d       to run it as a daemon\n", argv[0]);
+        
         // log system LOG_ERR
         syslog(LOG_ERR, "Error! Invalid number of arguments: %d", argc);
         exit(EXIT_FAILURE);
     }
     else 
     {
+        // Set flag for daemon process
         if ((argc == 2) && (strcmp( argv[1], "-d") == 0))
             bRunAsDaemon = true;
     }
-    syslog(LOG_DEBUG, "main()...bRunAsDaemon=%d", bRunAsDaemon);
+    DEBUG_LOG("main()...bRunAsDaemon=%d", bRunAsDaemon);
 
-    //printf("main()...set signal handler...\n");            // To be commentted out!!!
-    syslog(LOG_DEBUG, "main()...set signal handler...");
-
+    
     // Set sigaction to hanlde Signals
+    DEBUG_LOG("main()...set signal handler...");
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
     sa.sa_handler = handle_signal;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
 
-    // Register signal handlers for SIGTERM, SIGINT, SIGKILL, and SIGQUIT
-    if (sigaction(SIGTERM, &sa, NULL) != 0)
-    {
-        //printf("Error %d (%s) registering for SIGTERM\n", errno, strerror(errno));
-        syslog(LOG_ERR, "Failed registering for SIGTERM, errno %d (%s)", errno, strerror(errno));
-        //bSigHandlerSuccess = false;
-    }
-
-    if (sigaction(SIGINT, &sa, NULL) != 0)
-    {
-        //printf("Error %d (%s) registering for SIGINT\n", errno, strerror(errno));
-        syslog(LOG_ERR, "Failed registering for SIGINT, errno %d (%s)", errno, strerror(errno));
-        //bSigHandlerSuccess = false;
-    }
-
-    //if (sigaction(SIGKILL, &sa, NULL) != 0)                                           // We can not handle SIGKILL signal!!
-    //{
-    //    printf("Error %d (%s) registering for SIGKILL\n", errno, strerror(errno));
-    //    //bSigHandlerSuccess = false;
-    //}
-
-    if (sigaction(SIGQUIT, &sa, NULL) != 0)
-    {
-        //printf("Error %d (%s) registering for SIGQUIT\n", errno, strerror(errno));
-        syslog(LOG_ERR, "Failed registering for SIGQUIT, errno %d (%s)", errno, strerror(errno));
-        //bSigHandlerSuccess = false;
-    }
+    register_signal_handler(&sa);
 
 
-    // Opening file in writing mode
+    // Opening file in 'append' mode
+    DEBUG_LOG("main()...open output file...");
     pFile = fopen(OUTPUT_FILE, "a+");
 
-    if (pFile == NULL) {
-        //printf("Error! Unable to open file");
-        //syslog(LOG_ERR, "Error! Unable to open (or create) file %s", OUTPUT_FILE);
-        syslog(LOG_ERR, "Failed to open (or create file %s, errno %d (%s)", OUTPUT_FILE, errno, strerror(errno));
+    if (pFile == NULL)
+    {
+        syslog(LOG_ERR, "Failed to open (or create) file %s, errno %d (%s)", OUTPUT_FILE, errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    //printf("main()...set socket...\n");            // To be commentted out!!!
-    syslog(LOG_DEBUG, "main()...set socket...");
 
     // Creating socket file descriptor
+    DEBUG_LOG("main()...create and set server socket...");
     if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
-        //perror("socket failed");
         syslog(LOG_ERR, "Failed to create server socket, errno %d (%s)", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
@@ -226,37 +236,32 @@ int main(int argc, char *argv[])
     // Set some socket option
     if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
     {
-        //perror("setsockopt failed");
         syslog(LOG_ERR, "Failed to set socket options, errno %d (%s)", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
     
 
-    syslog(LOG_DEBUG, "main()...bind and listen to socket...");
-
     // Forcefully attaching socket to the port 9000
+    DEBUG_LOG("main()...bind and listen to socket...");
     sSockAddress.sin_family = AF_INET;
     sSockAddress.sin_addr.s_addr = INADDR_ANY;
     sSockAddress.sin_port = htons(PORT);
+
     if (bind(socket_fd, (struct sockaddr *) &sSockAddress, sizeof(sSockAddress)) < 0)
     {
-        //perror("bind failed");
         syslog(LOG_ERR, "Failed to bind socket on port %d, errno %d (%s)", PORT, errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
     if (listen(socket_fd, CONNECTION_QUEUE) < 0)
     {
-        //perror("listen failed");
         syslog(LOG_ERR, "Failed to listen on socket, errno %d (%s)", errno, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-
-    //printf("main()...set socket non-blocking...\n");            // To be commentted out!!!
-    syslog(LOG_DEBUG, "main()...set socket non-blocking...");
-
+    
     // Set the server socket to non-blocking mode
+    DEBUG_LOG("main()...set socket non-blocking...");
     int flags = fcntl(socket_fd, F_GETFL, 0);
     assert(flags != -1);
     fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK);
@@ -267,6 +272,7 @@ int main(int argc, char *argv[])
     {
         //printf("Main loop number= %d, new_socket= %d\n", loop_number, new_socket);
         syslog(LOG_DEBUG, "====> Main loop number= %d, new_socket= %d", loop_number, new_socket);
+        //DEBUG_LOG("main()...====> Main loop number= %d, new_socket= %d", loop_number, new_socket);
 
         if (new_socket == -1)
         {
@@ -274,28 +280,23 @@ int main(int argc, char *argv[])
             new_socket = accept(socket_fd, (struct sockaddr *) &sSockAddress, (socklen_t *) &addrlen);
             //printf("Main loop...Debug only...new_socket= %d\n", new_socket);
             syslog(LOG_DEBUG, "Main loop...Debug only...new_socket= %d", new_socket);
+            //DEBUG_LOG("main()...====> Main loop...new_socket= %d", new_socket);
 
             if (new_socket == -1)
-                //printf("Main loop...Debug only...on accept: Error %d (%s)\n", errno, strerror(errno));
                 syslog(LOG_ERR, "Failed on accept(), errno %d (%s)", errno, strerror(errno));
             else
             {
                 // Extract client IP address and port
-                client_ip[INET_ADDRSTRLEN] = {0};       // Re-init buffer
+                //client_ip[INET_ADDRSTRLEN] = {0};       // Re-init buffer
+                memset(client_ip, 0, sizeof(client_ip)); // clear buffer
                 inet_ntop(AF_INET, &(sSockAddress.sin_addr), client_ip, INET_ADDRSTRLEN);
                 client_port = ntohs(sSockAddress.sin_port);
 
-                //printf("New connection from %s:%d\n", client_ip, client_port);
                 syslog(LOG_DEBUG, "+++++++++++++++++++++++++++ New connection from %s:%d", client_ip, client_port);
-
-                // // Send a message to the client using the client socket
-                // char *message = "Hello, client!";
-                // send(new_socket, message, strlen(message), 0);
             }
         }
 
         if (new_socket >= 0)
-        //else
         {
             //printf("Main loop...Debug only...a client is connected, new_socket = %d\n", new_socket);
 
@@ -340,11 +341,7 @@ int main(int argc, char *argv[])
                 // TODO create File, open file, etc... and append to file
                 //  size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
                 //
-                            // for (int i = 0; i < valread; i++)
-                            // {
-                            //     bufferTmp[i] = buffer[i];
-                            // }
-                            // printf("After copy, bufferTmp contains     : %s\n", bufferTmp);
+        
 
                 // Write into the file
                 //int num_char_written = fprintf(pFile, "%s", recv_buffer);
@@ -379,8 +376,7 @@ int main(int argc, char *argv[])
                     //
                     syslog(LOG_DEBUG, "Send the full content of the file back to the client...");
 
-                    // // //send(new_socket, hello, strlen(hello), 0);
-                    // // //printf("Hello message sent\n");
+                    
                     // // int num_bytes_sent = 0;
                     // // //num_bytes_sent = send(new_socket, buffer, valread, 0);
                     // printf("Buffer contains     : %s\n", buffer);
@@ -475,34 +471,13 @@ int main(int argc, char *argv[])
             syslog(LOG_ERR, "Failed on usleep(), errno %d (%s)", errno, strerror(errno));
             //break;      // Exit while loop
         }
-    }
-
-
-    // TODO: Send data back to the client   (COMMENTED OUT TEMPORARY)
-    //      ssize_t send(int sockfd, const void *buf, size_t len, int flags);
-    // TODO : -check return value on send...
-    //send(new_socket, hello, strlen(hello), 0);
-    //printf("Hello message sent\n");
-    //printf("Prepare to send data back to the client...\n");
-
-    // printf("bufferTmp contains     : %s\n", bufferTmp);
-    // printf("strlen(bufferTmp)      : %ld\n", strlen(bufferTmp));
-
-    // int num_bytes_sent = 0;
-    // //num_bytes_sent = send(new_socket, buffer, valread, 0);
-    // num_bytes_sent = send(new_socket, bufferTmp, strlen(bufferTmp), 0);
-    // //num_bytes_sent = send(socket_fd, bufferTmp, strlen(bufferTmp), 0);
-    // printf("Num byte sent is: %d\n", num_bytes_sent);
-
-    // if (num_bytes_sent == -1)
-    //     printf("Main loop...Debug only...on send: Error %d (%s)\n", errno, strerror(errno));
+    }   // End while (running)
 
 
     // TODO: Cleanup before leaving
     //  close connection...
     //  closing any open sockets
     //  close file and deleting the file /var/tmp/aesdsocketdata.
-    //printf("Cleanup before exit...\n");
     syslog(LOG_DEBUG, "Cleanup before exit...");
     if (new_socket != -1)
     {
@@ -512,11 +487,10 @@ int main(int argc, char *argv[])
             
     if (close(socket_fd) != 0)
         syslog(LOG_ERR, "Failed closing server socket, errno %d (%s)", errno, strerror(errno));
-
     
     fclose(pFile);
-    // TODO : delete file...
-    //delete_output_file();
+    delete_output_file();
+
     print_signal_infos();
 
     return EXIT_SUCCESS;
